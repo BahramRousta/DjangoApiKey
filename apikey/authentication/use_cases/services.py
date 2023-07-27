40c8b9a2632db1873d025f8a7b674286027b1a2e
bahramrousta <rousta.bahram@gmail.com>
2023-07-27 19:07:46 +0330

    add create_api_key service

--- apikey/authentication/use_cases/services.py
+++ apikey/authentication/use_cases/services.py
@@ -1,5 +1,7 @@
 from apikey.authentication.models import ApiKey
+from apikey.company.interfaces import get_company_interface
 
 
 def create_api_key(company_id: int):
-    pass
+    company = get_company_interface(company_id=company_id)
+    return ApiKey.objects.create(company=company)
