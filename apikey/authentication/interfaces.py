803c264278507d8cc339ba4d314b927473a58319
bahramrousta <rousta.bahram@gmail.com>
2023-07-27 20:29:28 +0330

    fix api key validation

--- apikey/authentication/interfaces.py
+++ apikey/authentication/interfaces.py
@@ -1,10 +1,9 @@
+from rest_framework.exceptions import AuthenticationFailed
 from apikey.authentication.use_cases.selectors import get_api_key
 
 
 def validate_api_key_interface(api_key):
-
     api_key = get_api_key(api_key=api_key)
-    if not api_key:
-        return None
-
+    if api_key is None:
+        raise AuthenticationFailed('Invalid API key.')
     return api_key
