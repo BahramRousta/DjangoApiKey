651d9b62147d287fa82bbcb7d49eaf1949e996ae
bahramrousta <rousta.bahram@gmail.com>
2023-07-27 16:59:22 +0330

    add company model

--- apikey/company/models.py
+++ apikey/company/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from apikey.common.models import BaseModel
+
+
+class Company(BaseModel):
+
+    name = models.CharField(max_length=100, unique=True, db_index=True)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
